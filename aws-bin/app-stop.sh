deacf9b03ed63b31db2e68a1ae79bd8670ed2070
David Rueda <davd33@gmail.com>
2017-03-22 18:29:53 +0100

    Correction stop script.
    In case the app is not running, simply exit 0.

--- aws-bin/app-stop.sh
+++ aws-bin/app-stop.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-kill `pgrep -f '@angular/cli'` > /home/ec2-user/app-stop.log
+if pgrep -f '@angular/cli'; then
+  kill `pgrep -f '@angular/cli'` > /home/ec2-user/app-stop.log
+fi
+
+exit 0
