a9e42513f14a5184f7751bbff117580db1f6737f
David Rueda <davd33@gmail.com>
2017-03-23 11:50:43 +0100

    Nohup and wait 30s for running app

--- aws-bin/app-start.sh
+++ aws-bin/app-start.sh
@@ -2,4 +2,5 @@
 
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/lt-mvp-webapp
-npm start > /home/ec2-user/app-start.log
+nohup npm start > /home/ec2-user/app-start.log 2>&1
+sleep 30
--- aws-bin/app-validate.sh
+++ aws-bin/app-validate.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-echo "Service validated." > /home/ec2-user/app-validate.log
+if pgrep -f '@angular/cli'; then
+    exit 0
+fi
+
+"@angular/cli service not running." > /home/ec2-user/app-validate.log
+
+exit 1
