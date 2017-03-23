50064e00725a335430f8c15515b6cfecb233e437
David Rueda <davd33@gmail.com>
2017-03-23 11:58:39 +0100

    Start nohup in background.

--- aws-bin/app-start.sh
+++ aws-bin/app-start.sh
@@ -2,5 +2,5 @@
 
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/lt-mvp-webapp
-nohup npm start > /home/ec2-user/app-start.log 2>&1
+nohup npm start > /home/ec2-user/app-start.log 2>&1 &
 sleep 30
