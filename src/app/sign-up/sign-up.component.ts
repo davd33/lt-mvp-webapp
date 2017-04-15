f3589330fc3df1e9ec52a55a8f1838b5ddb8f647
David Rueda <davd33@gmail.com>
2017-04-15 15:01:44 +0200

    Captcha Client Validation on sign up component.

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -4,6 +4,8 @@ import {routerTransition} from '../router.animations';
 import {SignUpService} from "../services/sign-up.service";
 import {LangService} from "../services/lang.service";
 
+declare const grecaptcha: any;
+
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
@@ -13,16 +15,25 @@ import {LangService} from "../services/lang.service";
     routerTransition()
   ]
 })
-export class SignUpComponent implements AfterViewInit {
+export class SignUpComponent implements OnInit, AfterViewInit {
 
   email: string;
   price: number;
+
   registrationSuccess: boolean = false;
-  @ViewChild('input') inputChild;
   emailRegex: RegExp = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  captchaDone: boolean = false;
+
+  @ViewChild('input') inputChild;
+  @ViewChild('captcha') captchaChild;
+  @ViewChild('signUpForm') formChild;
 
   constructor(private signUpService: SignUpService,
               private lang: LangService) {
+    window['onloadCallback'] = this.captchaLoad.bind(this);
+  }
+
+  ngOnInit() {
   }
 
   ngAfterViewInit() {
@@ -30,18 +41,31 @@ export class SignUpComponent implements AfterViewInit {
   }
 
   sendForm() {
-    if (this.email && this.email.trim() !== "")
+    if (this.formChild.valid && this.captchaDone) {
       this.signUpService.registerMail(this.email.trim(), this.price)
         .then((res) => {
           console.log(res);
           this.registrationSuccess = true;
         });
+    }
   }
 
   keyValid(event: any) {
     if (event.key == "Enter") {
       this.sendForm();
     }
+  }
+
+  captchaLoad() {
+    let captchaChildElmt = this.captchaChild.nativeElement;
+
+    grecaptcha.render(captchaChildElmt, {
+      'sitekey': '6LfAHR0UAAAAANvcs6MfnYzUMeJE-V3MhNaTfQNt',
+      'callback': (res) => {
+        this.captchaDone = true;
+      },
+      'theme': 'light'
+    });
   }
 
 }
