99c760edeef6169f22e32a7e583a8e0b9149da44
David Rueda <davd33@gmail.com>
2017-04-28 10:38:59 +0200

    downgrade zone.js after installing packages
    zone js bug:
    https://github.com/angular/zone.js/issues/746

--- aws-bin/install-pkgs.sh
+++ aws-bin/install-pkgs.sh
@@ -3,3 +3,6 @@
 source /home/ec2-user/.bash_profile
 cd /home/ec2-user/lt-mvp-webapp
 npm install > install-pkgs.log
+
+# zone js bug: https://github.com/angular/zone.js/issues/746
+npm install zone.js@0.8.7 > install-pkgs.zonejs.log
