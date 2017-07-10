21039b396dfa338c97836b0debbc88ea83657aff
David Rueda <davd33@gmail.com>
2017-07-10 11:32:33 +0200

    mock tests - enhance explanations

--- src/app/mock-tests.ts
+++ src/app/mock-tests.ts
@@ -9,26 +9,26 @@ export const LT_LIST: any[] = [
       }, {
         'value': 'in',
         'isInput': true,
-        'explanation': 'You should use the accusative form of \'in\'.',
+        'explanation': `PREPOSITION 'in' A/D + P/S + F/M/N`, //`Germany is a neutral name. When you want to say that you live or do something in a particular country (or city) that requires the neutral article in German, you simply use the preposition 'in'.`,
         'complete': true
       }, {
         'value': ' Deutschland studieren! Leider habe ich soweit k',
       }, {
         'value': 'ein',
         'isInput': true,
-        'explanation': 'There is not...'
+        'explanation': `The declination form of indefinite neutral accusative articles is 'ein' (das App).`
       }, {
         'value': ' cool',
       }, {
         'value': 'es',
         'isInput': true,
-        'explanation': 'This adjective needs to be accorded: neutral / accusative.'
+        'explanation': `The adjective (cool) is before the substantive (App). It then needs an ending form: the declination form of indefinite article nominative neutral is 'es'.`
       }, {
         'value': ' App gefunden um Lückentexte ',
       }, {
         'value': 'zu',
         'isInput': true,
-        'explanation': 'Use the form um[...]zu.',
+        'explanation': `Use the form um[...]zu, they are best friends and they are always together.`,
         'complete': true
       }, {
         'value': ' üben!',
@@ -45,31 +45,31 @@ export const LT_LIST: any[] = [
       }, {
         'value': 'me',
         'isInput': true,
-        'explanation': 'I come... in German!',
+        'explanation': `The declination form of the first person in singular for the verb 'kommen' it's komm-e-.`,
       }, {
         'value': ' aus Frankreich. Ich möcht',
       }, {
         'value': 'e',
         'isInput': true,
-        'explanation': 'I would like... subjunctive of want.'
+        'explanation': `The declination form of the first person in singular for the verb 'möchten'(would like) is möcht-e-`
       }, {
         'value': ' in Deutschland studieren! Leider hab',
       }, {
         'value': 'e',
         'isInput': true,
-        'explanation': 'First person.'
+        'explanation': `The declination form of the first person in singular for the verb 'haben' (have) is hab-e-`
       }, {
         'value': ' ich soweit kein cooles App gefund',
       }, {
         'value': 'en',
         'isInput': true,
-        'explanation': 'Found!',
+        'explanation': `The past form of the verb 'finden' (find) is gefunden, `,
       }, {
         'value': ' um Lückentexte zu üb',
       }, {
         'value': 'en',
         'isInput': true,
-        'explanation': 'Infinitive of this verb.',
+        'explanation': `Infinitive of this verb.`,
       }, {
         'value': '!',
       }
@@ -85,28 +85,28 @@ export const LT_LIST: any[] = [
       }, {
         'value': 'in',
         'isInput': true,
-        'explanation': 'I wanted to...',
+        'explanation': `Germany is a neutral name. When you want to say that you live or do something in a particular country (or city) that requires the neutral article in German, you simple use the preposition 'in'.`,
         'complete': true
       }, {
         'value': ' Deutschland studieren, aber ',
       }, {
         'value': 'im',
         'isInput': true,
-        'explanation': '\'in dem\'',
+        'explanation': `Internet is a neutral name (das). The preposition 'in' can be used in dative or accusative form. Since there is no idea of movement in our phrase the right form to be used in this case is the dative: 'in dem'.`,
         'complete': true
       }, {
         'value': ' Internet gab es leider kein cooles App um ',
       }, {
         'value': 'die',
         'isInput': true,
-        'explanation': 'Plural preposition.',
+        'explanation': `We are searching for a preposition that designate a plural substantive: 'Lückentexte'.`,
         'complete': true
       }, {
         'value': ' blöde Lückentexte ',
       }, {
         'value': 'zu',
         'isInput': true,
-        'explanation': 'um...zu',
+        'explanation': `The final clause is to be completed. The form 'um [...] zu' answers to the question 'To what purpose?'. Example: "Ich lerne fleißig Deutsch, um in Deutschland zu studieren."`,
         'complete': true
       }, {
         'value': ' üben!',
@@ -123,25 +123,25 @@ export const LT_LIST: any[] = [
       }, {
         'value': 'e',
         'isInput': true,
-        'explanation': 'I wanted...',
+        'explanation': `The first singular person of the verb 'want'. The verb is conjugated in the past. Example: "Ich wollte lernen" ("I wanted to learn").`,
       }, {
         'value': ' in Deutschland studier',
       }, {
         'value': 'en',
         'isInput': true,
-        'explanation': 'Infinitive of this verb.'
+        'explanation': `Infinitive of the verb 'study'. This verb precise the action that the main verb asks for. Example: "I want what? I want to study".`
       }, {
         'value': ', aber im Internet g',
       }, {
         'value': 'ab',
         'isInput': true,
-        'explanation': 'Geben... preterit.'
+        'explanation': `The third singular person of the verb 'give' in the past (preterit).`
       }, {
         'value': ' es leider kein App um die blöde Lückentexte zu üb',
       }, {
         'value': 'en',
         'isInput': true,
-        'explanation': 'Infinitive of this verb.',
+        'explanation': `Infinitive of this verb.`,
       }, {
         'value': '!',
       }
