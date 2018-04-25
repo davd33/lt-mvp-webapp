910ba1338dec8b22e5f5b1d384b65242f81f3fa4
David Rueda <davd33@gmail.com>
2018-04-25 14:05:13 +0200

    fix path reload 404 error

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 //Install express server
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'))
+
+app.get('*', function(req, res) {
+  res.sendfile(__dirname + '/dist/index.html')
+})
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 8080
+app.listen(port)
+
+console.log(`Listening on ${port}`)
