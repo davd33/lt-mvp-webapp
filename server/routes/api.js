7fb9e74695deb655448093b3eee428f28358a139
David Rueda <davd33@gmail.com>
2018-05-12 05:46:39 +0700

    es - get 10 random lt

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,12 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const elasticsearch = require('elasticsearch')
 
+
+if (!process.env.ES_URL) {
+  console.error("Env var missing: ES_URL")
+  process.exit(1)
+}
+
+const es = new elasticsearch.Client({
+  host: process.env.ES_URL
+});
 
 /**
  * send back a random list of lucken tests
  */
 router.get('/lt/randoms', (req, res) => {
-  res.send('api works');
+
+  es
+    .search({
+      index: 'lt',
+      body: {
+        "size": 10,
+        "query": {
+          "function_score": {
+            "functions": [
+              {
+                "random_score": {
+                  "seed": new Date().getTime()
+                }
+              }
+            ]
+          }
+        }
+      }
+    })
+    .then(function (body) {
+
+      res.send(body);
+
+    })
+    .catch(function (error) {
+      console.trace(error.message);
+      res.send(error)
+    });
 });
 
 module.exports = router;
