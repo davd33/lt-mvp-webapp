f262080b2397b604250fe2c61d0d0c19e79a4256
David Rueda <davd33@gmail.com>
2018-05-22 14:42:34 +0700

    init sun

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -14,28 +14,12 @@ export class IndexComponent implements OnInit {
 
   @ViewChild('topSection') topSectionEl: ElementRef
 
-  private windowHeight: number
-
   constructor(private titleService: Title,
-              private mouse: MouseService,
-              private window: WindowService,
               public lang: LangService) {
   }
 
   ngOnInit() {
     this.titleService.setTitle(this.lang.text.Global.title)
-
-    this.windowHeight = window.innerHeight
-    this.window.obs.subscribe(value => {
-      this.windowHeight = value.target.innerHeight
-    })
-
-    this.mouse.obs.subscribe(value => {
-      const y = value.pageY
-      let windowPercent = 1 - (y / this.windowHeight)
-      this.topSectionEl.nativeElement.style.opacity = `${windowPercent < 0 ? 0 : windowPercent}`
-      this.topSectionEl.nativeElement.style.transform = `translateY(${y/25}vh)`
-    })
   }
 
 
